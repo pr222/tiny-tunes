d40c4be7aa3219262b3177e26eb7c613871bca39
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-11-09 17:19:09 +0100

    Complete Exercise 1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
  *
  * @author Johan Leitet <johan.leitet@lnu.se>
  * @author Mats Loock <mats.loock@lnu.se>
- * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
  * @version 1.0.0
  */
 
 // TODO: Write your code here!
+const hello = document.getElementById('step01_hello')
+
+hello.textContent = 'Hello world!'
